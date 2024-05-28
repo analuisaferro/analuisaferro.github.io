a1f68023e7fdf85ae5c14fc852371f8bf6662468
Ana Luísa Ferro <analuisaferro@gmail.com>
2024-05-28 17:43:51 -0300

    oi

--- teste/assets/js/script.js
+++ teste/assets/js/script.js
@@ -1,9 +1,16 @@
 function showNav(){
+    let icon = document.querySelector(".icon-nav-mobile")
     let menu = document.querySelector(".mobile-menu")
     console.log()
     if(menu.classList.contains('open')){
         menu.classList.remove('open')
+        icon.classList.add('fa-bars')
+        icon.classList.remove('fa-x')
+
     }else{
         menu.classList.add('open')
+        icon.classList.remove('fa-bars')
+        icon.classList.add('fa-x')
+        
     }
 }
